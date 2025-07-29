e8e8009e930160eae288a476e753254b78c2985e
Jose Pereira <jrpereira@scl.edu.gt>
2025-07-29 11:05:00 -0600

    Rama Jose

--- Componentes/modulos/item/itemModulo.js
+++ Componentes/modulos/item/itemModulo.js
@@ -4,6 +4,23 @@ export function item(){
     
     item.className = "item"
 
+    let titulo = document.createElement("h2")
+    titulo.textContent = "Titulo";
+    item.appendChild(titulo);
+
+    let img = document.createElement("img")
+    img.src = "../assets/Camaro.jpg"
+    img.className = "Camaro"
+    item.appendChild(img);
+
+    let descripcion = document.createElement("descripcion")
+    descripcion.textContent = "El Chevrolet Camaro fue creado por General Motors en 1966 como respuesta directa al éxito del Ford Mustang. Diseñado como un auto deportivo y accesible, debutó en 1967 con opciones de motor V6 y V8. Su estilo agresivo y rendimiento lo convirtieron rápidamente en un ícono de los muscle cars."
+    descripcion.className = "historia"
+    item.appendChild(descripcion);
+
+
+
+
 
     return item;
 }
